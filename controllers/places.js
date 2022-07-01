809ca6f9c58be639c4bfffeb580f9cbefe99ca6d
Jay Holt <jholt3@Jays-MacBook-Pro.local>
2022-07-01 11:31:58 -0400

    Adding places view with placeholder data

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,22 @@
-const router = require('express').Router()
+const app = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'Pizzeria Toro',
+        city: 'Durham',
+        state: 'North Carolina',
+        cuisines: 'Italian, Pizza',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Moonshadows',
+        city: 'Malibu',
+        state: 'California',
+        cuisines: 'New American',
+        pic: 'http://placekitten.com/250/250'
+    }]
+    res.render('places/index', {places})
 })
 
-module.exports = router
+module.exports = app
 
