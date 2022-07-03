15cfd9ded2fd98dbb966809b95ac5bac2f9ee274
Jay Holt <johnpholt3@gmail.com>
2022-07-03 13:20:58 -0400

    Updating index image alignment

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,12 +3,14 @@ const Def = require('../default')
 
 function index (data) {
     let placesFormatted = data.places.map((place) => {
-        return (<div className='col-sm-6'>
-            <h2>{place.name}</h2>
+        return (
+        <div className='col-sm-6'>
+            <h2 className='text-center'>
+                {place.name}</h2>
             <p className='text-center'>
                 {place.cuisines}
             </p>
-            <img src={place.pic} alt={place.name} />
+            <img src={place.pic} class="mx-auto d-block" alt={place.name} />
             <p className='text-center'>
                 Located in {place.city}, {place.state}
             </p>
@@ -20,7 +22,7 @@ function index (data) {
             <main>
                 <h1>Places to RANT or RAVE About</h1>
                 <div className='row'>
-                {[placesFormatted]}
+                {placesFormatted}
                 </div>
             </main>
         </Def>
