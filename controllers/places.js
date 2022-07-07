736462156e2d81c71becc06f4ab4fb812542a72e
Jay Holt <johnpholt3@gmail.com>
2022-07-07 08:51:34 -0400

    Added edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,14 +1,29 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+// GET / ('Home Page')
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
-
 // GET /places/new
 router.get('/new', (req, res) => {
     res.render('places/new')
+})
+
+
+// GET /places/edit
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else{
+        res.render('places/edit', {place: places[id]})
+    }
 })
 
 // GET /places/show
@@ -25,20 +40,6 @@ router.get('/:id', (req, res) => {
     }
 })
 
-// GET /places/edit
-router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else{
-        res.render('places/edit', {place: places[id]})
-    }
-})
-
 // POST /places 
 router.post('/', (req, res) => {
     if (!req.body.pic) {
@@ -52,6 +53,30 @@ router.post('/', (req, res) => {
     }
     places.push(req.body)
     res.redirect('/places')
+})
+
+// PUT /places
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else{
+        if (!req.body.pic) {
+            req.body.pic = 'http://placekitten.com/250/250'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
 })
 
 // DELETE /places
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -14,19 +14,19 @@ function edit_form (data) {
                     </div>
                     <div className='form-group'>
                         <label htmlFor='pic'>Place Picture</label>
-                        <input className='form-control' type='url' id='pic' name='pic' />
+                        <input className='form-control' type='url' id='pic' name='pic' value={data.place.pic} />
                     </div>
                     <div className='form-group'>
                         <label htmlFor='city'>City</label>
-                        <input className='form-control' id='city' name='city' />
+                        <input className='form-control' id='city' name='city' value={data.place.city} />
                     </div>
                     <div className='form-group'>
                         <label htmlFor='state'>State</label>
-                        <input className='form-control' id='state' name='state' />
+                        <input className='form-control' id='state' name='state' value={data.place.state}/>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='cuisines'>Cuisines</label>
-                        <input className='form-control' id='cuisines' name='cuisines' required />
+                        <input className='form-control' id='cuisines' name='cuisines' value={data.place.cuisines}required />
                     </div>
                     <input className='btn-primary' type='submit' value='Add Place' />
                 </form>
