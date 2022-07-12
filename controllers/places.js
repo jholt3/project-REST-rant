4060a6067b033301d50d9823fa242961fb97cb3b
Jay Holt <johnpholt3@gmail.com>
2022-07-11 21:23:06 -0400

    Updated Show page and links

--- controllers/places.js
+++ controllers/places.js
@@ -1,100 +1,63 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
-// GET /places
 router.get('/', (req, res) => {
-    res.render('places/index', { places })
+  db.Place.find()
+    .then((places => {
+        res.render('places/index', {places})
+    }))
+    .catch(err => {
+        console.log(err)
+        res.render('error404')
+    })
 })
 
-// POST /places 
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-        req.body.pic='http://placekitten.com/250/250'
-    }
-    if (!req.body.city) {
-        req.body.city='Anytown'
-    }
-    if (!req.body.state) {
-        req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
+    db.Place.create(req.body)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
-// GET /places/new
 router.get('/new', (req, res) => {
-    res.render('places/new')
+  res.render('places/new')
 })
 
-// GET /places/show
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else{
-        res.render('places/show', {place: places[id], id})
-    }
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/show', {place})
+  })
+  .catch(err => {
+    console.log('err, err')
+    res.render('error404')
+  })
 })
 
-// PUT /places
 router.put('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else{
-        if (!req.body.pic) {
-            req.body.pic = 'http://placekitten.com/250/250'
-        }
-        if (!req.body.city) {
-            req.body.city = 'Anytown'
-        }
-        if (!req.body.state) {
-            req.body.state = 'USA'
-        }
-        places[id] = req.body
-        res.redirect(`/places/${id}`)
-    }
+  res.send('PUT /places/:id stub')
 })
 
-// DELETE /places
 router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        places.splice(id, 1)
-        res.redirect('/places')
-    }
+  res.send('DELETE /places/:id stub')
 })
 
-// GET /places/edit
 router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else{
-        res.render('places/edit', {place: places[id], id})
-    }
+  res.send('GET edit form stub')
 })
 
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
 
-
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
+
 
--- models/places.js
+++ models/places.js
@@ -1,16 +1,13 @@
 // GET /places
-module.exports = [{
-    name: 'Za Pizzeria',
-    city: 'Durham',
-    state: 'North Carolina',
-    cuisines: 'Italian, Pizza',
-    pic: '/images/pizza.jpg'
-    // Photo by <a href="https://unsplash.com/@saundiii?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Saundarya Srinivasan</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-}, {
-    name: 'Oceanview Restaurant',
-    city: 'Malibu',
-    state: 'California',
-    cuisines: 'New American',
-    pic: '/images/beach-lunch.jpg' 
-    // Photo by <a href="https://unsplash.com/@fredasem?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Freddy Kearney</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    pic: String, 
+    cuisines: {type: String, required: true},
+    city: {type: String, default: 'Anytown'},
+    state: {type: String, default: 'USA'},
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
