f2ef5c14a0400dfa20b98318b198989ca46f12f7
Jay Holt <johnpholt3@gmail.com>
2022-07-13 22:53:30 -0400

    Utilized Helper Method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,28 +2,36 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
+    return(
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                
-                <div>
-                    <img src={data.place.pic}/>
-                </div>
-                <div>
-                    <h2>Rating</h2>
-                    <p>Not rated</p>
-                </div>
-                <div>
-                    <h2>Description</h2>
-                    <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
-                </div>
-                <div>
-                    <h2>Comments</h2>
-                    <p>No comments</p>
-                </div>
-               
-               <div className='buttons'>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name}/>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className='col-sm-6'>
+                    <h1>
+                        {data.place.name}
+                    </h1>
+                    <h2>
+                        Rating
+                    </h2>
+                    <p> 
+                        Not rated
+                    </p>
+                    <h2>
+                        Description
+                    </h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
+                    <div className='buttons'>
                <ul>
                     <li>
                         <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
@@ -35,10 +43,17 @@ function show (data) {
                     </li>
                 </ul>
                </div>
+         </div>
+                    <h2>Comments</h2>
+                    <p>No comments</p>
+                </div>
                 
             </main>
         </Def>
     )
 }
+    
+        
+   
 
 module.exports = show
