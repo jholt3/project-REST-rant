74830301467a04c2d45b36cd80f24fad1f94fe05
Jay Holt <johnpholt3@gmail.com>
2022-07-23 22:09:42 -0400

    Updated edit and delete functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,14 +7,32 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
+
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars}, 0)
+        let averageRating = Math.round(sumRatings/data.place.comments.length)
+        let stars = ''
+        for (let i=0; i<averageRating; i++) {
+            stars += '⭐️'
+        }
+        rating = (
+        <h3>
+        {stars} stars
+        </h3>
+        )
         comments = data.place.comments.map(c => {
             return(
                 <div className='border'>
                     <h2 className='rant'>{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
                     <h4>{c.content}</h4>
                     <h3>
-                        <stong>- {c.author}</stong>
+                        <strong>- {c.author}</strong>
                     </h3>
                     <h4>Rating: {c.stars}</h4>
                 </div>
@@ -38,9 +56,8 @@ function show (data) {
                     <h2>
                         Rating
                     </h2>
-                    <p> 
-                        Not rated
-                    </p>
+                    {rating}
+                    <br />
                     <h2>
                         Description
                     </h2>
@@ -56,16 +73,40 @@ function show (data) {
                         <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
                     </li>
                     <li>
-                        <form method='POST' action={`/places/${data.id}?_method=DELETE`} >
+                        <form method='POST' action={`/places/${data.place.id}?_method=DELETE`} >
                             <button type='submit' className='btn btn-danger'>Delete </button>
                         </form>
                     </li>
                 </ul>
-               </div>
-         </div>
-                    <h2>Comments</h2>
-                    {comments}
                 </div>
+                </div>
+                </div>
+
+                <h2>Comments</h2>
+                    {comments}
+                <div>
+                <form method='POST' action={`/places/${data.place.id}?_method=PUT`}>
+                    <div className='form-group'>
+                        <label htmlFor='author'>Author</label>
+                        <input className='form-control' id='author' name='author' />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='content'>Content</label>
+                        <input className='form-control' id='content' name='content' />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='stars'>Star Rating</label>
+                        <input type='range' id='stars' name='stars' min='0' max='5'/>
+                    </div>
+                    <div>
+                        <input type='checkbox' id='rant' name='rant' value='rant'/>
+                        <label for='rant'>Rant?</label>
+                    </div>
+
+                    <input className='btn-primary' type='submit' value='Add Comment' />
+                </form>
+                </div>
+    
                 
             </main>
         </Def>
