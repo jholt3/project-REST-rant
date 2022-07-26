3a55de3fea8679a59c8bae8d10bf321f00a92849
Jay Holt <johnpholt3@gmail.com>
2022-07-26 19:53:17 -0400

    defined dotenv in index.js file

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+
+dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
